8df58e7c1035584a94e84d9fa36c4dc62e9829e9
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2023-11-12 04:01:15 +0100

    textField + MUI

--- front-app-volunteer/src/components/textField/textField.js
+++ front-app-volunteer/src/components/textField/textField.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './textField.css'; // Importer le fichier CSS
+import { styled } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
 
-export default function TextField() {
+
+export default function textField() {
+
+    const CssTextField = styled(TextField)({
+        '& label.Mui-focused': {
+          color: '#002663',
+        },
+        '& label': {
+            color: '#4C5CC5',
+          },
+        '& .MuiInput-underline:after': {
+          borderBottomColor: '#4C5CC5',
+        },
+        '& .MuiOutlinedInput-root': {
+          '& fieldset': {
+            borderColor: '#4C5CC5',
+          },
+          '&:hover fieldset': {
+            borderColor: '#4C5CC5',
+          },
+          '&.Mui-focused fieldset': {
+            borderColor: '#002663',
+          },
+        },
+      });
+
   return (
     <div className='TextField'>
-        test
+        <CssTextField />
     </div>
   );
 }
