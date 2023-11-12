2a21bf85756a38fe0a98c76918d300965777a216
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2023-11-12 02:02:13 +0100

    Navbar avec effet hover

--- front-app-volunteer/src/components/navBar/navBar.js
+++ front-app-volunteer/src/components/navBar/navBar.js
@@ -1,17 +1,19 @@
+// NavBar.js
+
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './navBar.css'; // Importer le fichier CSS
 
-export default function navBar({ }) {
-
-
-    return(
-        <div className="navBar">
-            <a href="#accueil">Infos</a>
-            <a href="#aPropos">Planning</a>
-            <a href="#contact">Inscription</a>
-            <a href="#contact">Messagerie</a>
-            <a href="#contact" className='monProfil'>Mon Profil</a>
-        </div>
-    )
-
+export default function NavBar() {
+  return (
+    <div className='navBar'>
+        <nav className='navigation'>
+            <NavLink className="current" to="/"> Informations </NavLink>
+            <NavLink className="current" to="/"> Planning </NavLink>
+            <NavLink className="current" to="/"> Inscription </NavLink>
+            <NavLink className="current" to="/"> Messagerie </NavLink>
+            <NavLink className="monprofil" to="/"> Mon Profil </NavLink>
+        </nav>
+    </div>
+  );
 }
