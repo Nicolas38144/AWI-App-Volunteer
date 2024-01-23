e9873dbcfcf62271a1230b565c00298381e26ef0
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2024-01-23 23:01:38 +0100

    little change on navBar

--- front-app-volunteer/src/components/navBar/navBar.js
+++ front-app-volunteer/src/components/navBar/navBar.js
@@ -14,15 +14,15 @@ export default function NavBar({ hasReceivedMessages, hasNews, setVal }) {
 
   return (
     <div className='navBar'>
-      <NavLink id="nav-menu-button" to='/general' onClick={() => console.log("affiche le menu")}> Menu </NavLink>
+      <NavLink id="nav-menu-button" to='/home' onClick={() => console.log("affiche le menu")}> Menu </NavLink>
         <nav className='navigation'>
-            <Link onClick={() => setVal(0)} to={'/general'}><img src={logo} className='logo' alt='logo'></img></Link>
-            <NavLink className="current" to='/general' onClick={() => setVal(0)}> Informations {hasNews ? <span className='notification'></span> : ''}</NavLink>
-            <NavLink className="current" to='/general' onClick={() => setVal(1)}> Planning </NavLink>
-            <NavLink className="current" to='/general' onClick={() => setVal(2)}> Inscription </NavLink>
-            <NavLink className='current' to='/general' onClick={() => setVal(3)}> Messagerie {hasReceivedMessages ? <span className='notification'></span> : ''}</NavLink>
-            <NavLink className="current" to='/general' onClick={() => setVal(4)}> Forum </NavLink>
-            <NavLink className="monprofil" to='/general' onClick={() => setVal(5)}> Mon Profil </NavLink>
+            <Link onClick={() => setVal(0)} to={'/home'}><img src={logo} className='logo' alt='logo'></img></Link>
+            <NavLink className="current" to='/home' onClick={() => setVal(0)}> Informations {hasNews ? <span className='notification'></span> : ''}</NavLink>
+            <NavLink className="current" to='/home' onClick={() => setVal(1)}> Planning </NavLink>
+            <NavLink className="current" to='/home' onClick={() => setVal(2)}> Inscription </NavLink>
+            <NavLink className='current' to='/home' onClick={() => setVal(3)}> Messagerie {hasReceivedMessages ? <span className='notification'></span> : ''}</NavLink>
+            <NavLink className="current" to='/home' onClick={() => setVal(4)}> Forum </NavLink>
+            <NavLink className="monprofil" to='/home' onClick={() => setVal(5)}> Mon Profil </NavLink>
             <IconButton color="primary" aria-label="Se déconnecter" variant="outlined" onClick={logout}
             sx={{position: 'relative', left: '14vw',
             '&:hover': {
