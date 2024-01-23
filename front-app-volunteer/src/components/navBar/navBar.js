9efc05dc286abd18a701a4ef74a23230933e73ed
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2024-01-23 21:11:55 +0100

    logout

--- front-app-volunteer/src/components/navBar/navBar.js
+++ front-app-volunteer/src/components/navBar/navBar.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import './navBar.css'; // Importer le fichier CSS
 import logo from '../../images/logo31_couleur.png'
 import LogoutIcon from '@mui/icons-material/Logout';
 import IconButton from '@mui/material/IconButton';
 
 
+
 export default function NavBar({ hasReceivedMessages, hasNews, setVal }) {
+
+  const navigate = useNavigate();
+  const logout = () => { navigate('/'); };
+
   return (
     <div className='navBar'>
       <NavLink id="nav-menu-button" to='/general' onClick={() => console.log("affiche le menu")}> Menu </NavLink>
@@ -18,13 +23,13 @@ export default function NavBar({ hasReceivedMessages, hasNews, setVal }) {
             <NavLink className='current' to='/general' onClick={() => setVal(3)}> Messagerie {hasReceivedMessages ? <span className='notification'></span> : ''}</NavLink>
             <NavLink className="current" to='/general' onClick={() => setVal(4)}> Forum </NavLink>
             <NavLink className="monprofil" to='/general' onClick={() => setVal(5)}> Mon Profil </NavLink>
-            <IconButton color="primary" aria-label="Se déconnecter" variant="outlined"
+            <IconButton color="primary" aria-label="Se déconnecter" variant="outlined" onClick={logout}
             sx={{position: 'relative', left: '14vw',
             '&:hover': {
               color: 'rgb(0, 38, 99)',
             },}}>
               <LogoutIcon />
-            </IconButton>
+            </IconButton >
         </nav>
     </div>
   );
