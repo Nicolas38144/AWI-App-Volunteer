4af73bbba9f90c1fe2f6d46f5627182659f9405d
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2024-02-03 00:08:51 +0100

    planning intelligent

--- front-app-volunteer/src/views/planningView/planningView.js
+++ front-app-volunteer/src/views/planningView/planningView.js
@@ -12,9 +12,11 @@ export default function PlanningView(props){
       const [postes, setPostes] = useState([]);
       const [zones, setZones] = useState([]);
       const [plages, setPlages] = useState([]);
+      const [jours, setJours] = useState([]);
+      const joursDeLaSemaine = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
 
 
-      const getData = async () => { 
+      const getData = async () => {
         // get zones
         if (localStorage.getItem('zones') == null || typeof(localStorage.getItem('zones')) == 'undefined') {
           try {
@@ -52,14 +54,30 @@ export default function PlanningView(props){
       }
 
       // get plages
-      if (localStorage.getItem('plages') == null || typeof(localStorage.getItem('plages')) == 'undefined') {
+      if (localStorage.getItem('plages') === null || typeof(localStorage.getItem('plages')) == 'undefined') {
         try {
             const querySnapshot = await getDocs(collection(db, "plage_horaire"));
             var listPlage = []
+            var listJours = new Set()
             querySnapshot.forEach((doc) => {
                 listPlage.push(doc.data())
+                listJours.add(doc.data().jour)
+            });
+
+            const joursNonOrdonnesArray = Array.from(listJours);
+            const joursOrdonnesResultat = joursNonOrdonnesArray.sort((a, b) => {
+              return joursDeLaSemaine.indexOf(a) - joursDeLaSemaine.indexOf(b);
+            });
+
+            console.log(joursNonOrdonnesArray)
+            
+            joursOrdonnesResultat.forEach((unjour) => {
+              jours.push({"jour": unjour})
             });
             setPlages(listPlage)
+
+
+            localStorage.setItem('jours', JSON.stringify(jours));
             localStorage.setItem('plages', JSON.stringify(listPlage));
         } catch (error) {
             console.error('Error fetching postes data:', error);
@@ -67,11 +85,12 @@ export default function PlanningView(props){
       }
       else {
           setPlages(JSON.parse(localStorage.getItem('plages')));
+          setJours(JSON.parse(localStorage.getItem('jours')));
       }
+
+      console.log(JSON.parse(localStorage.getItem('jours')))
     }
       
-    
-    console.log(plages)
     return(
         <div className='planningView'>
             <h1>PlanningView</h1>
@@ -79,27 +98,30 @@ export default function PlanningView(props){
             <table>
                 <thead>
                     <th className='borderright'></th>
+                    {jours.map((unjour) => (
+                    <>
                     <th className='borderleft'></th>
                     <th></th>
-                    <th>Samedi</th>
+                    <th>{unjour.jour}</th>
                     <th></th>
                     <th className='borderright'></th>
-                    <th className='borderleft'></th>
-                    <th></th>
-                    <th>Dimanche</th>
+                    </>
+                    
+                    ))}
                 </thead>
                 <thead>
                     <tr>
                     <th className='quadrille'>Poste</th>
-                        <th className='quadrille'>9h-11h</th>
+                    {jours.map(() => (
+                    <>
+                    <th className='quadrille'>9h-11h</th>
                         <th className='quadrille'>11h-14h</th>
                         <th className='quadrille'>14h-17h</th>
                         <th className='quadrille'>17h-20h</th>
                         <th className='quadrille'>20h-22h</th>
-                        <th className='quadrille'>9h-11h</th>
-                        <th className='quadrille'>11h-14h</th>
-                        <th className='quadrille'>14h-17h</th>
-                        <th className='quadrille'>17h-20h</th>
+                    </>
+                    
+                    ))}
                     </tr>
                 </thead>
                 <tbody>
@@ -111,30 +133,36 @@ export default function PlanningView(props){
                 </tbody>
             </table>
 
+            {jours ? 
             <table>
                 <thead>
-                    <th className='borderright'></th>
+                <tr >
+                <th className='borderright'></th>
+                    {jours.map((unjour) => (
+                    <>
                     <th className='borderleft'></th>
                     <th></th>
-                    <th>Samedi</th>
+                    <th>{unjour.jour}</th>
                     <th></th>
                     <th className='borderright'></th>
-                    <th className='borderleft'></th>
-                    <th></th>
-                    <th>Dimanche</th>
+                    </>
+                    
+                    ))}
+                    </tr>
                 </thead>
                 <thead>
                     <tr>
                     <th className='quadrille'>Zones bénévoles</th>
+                    {jours.map(() => (
+                    <>
                     <th className='quadrille'>9h-11h</th>
-                    <th className='quadrille'>11h-14h</th>
-                    <th className='quadrille'>14h-17h</th>
-                    <th className='quadrille'>17h-20h</th>
-                    <th className='quadrille'>20h-22h</th>
-                    <th className='quadrille'>9h-11h</th>
-                    <th className='quadrille'>11h-14h</th>
-                    <th className='quadrille'>14h-17h</th>
-                    <th className='quadrille'>17h-20h</th>
+                        <th className='quadrille'>11h-14h</th>
+                        <th className='quadrille'>14h-17h</th>
+                        <th className='quadrille'>17h-20h</th>
+                        <th className='quadrille'>20h-22h</th>
+                    </>
+                    
+                    ))}
                     </tr>
                 </thead>
                 <tbody>
@@ -145,6 +173,7 @@ export default function PlanningView(props){
                     ))}
                 </tbody>
             </table>
+            :''}
             </div>
         </div>
     );
