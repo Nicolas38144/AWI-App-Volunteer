c437c15b91a62ea157cd4dc66918c42d0f2fed57
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2024-02-03 16:11:22 +0100

    data loaded in homeview

--- front-app-volunteer/src/views/homeView/homeView.js
+++ front-app-volunteer/src/views/homeView/homeView.js
@@ -21,6 +21,123 @@ export default function HomeView(props){
     const [games, setGames] = useState([]);
     const [countUsers, setCountUsers] = useState([]);
     const [actualUser, setActualUser] = useState(null);
+    const [postes, setPostes] = useState([]);
+    const [affectations_z, setAffectations_z]=useState([]) // affectations zones
+    const [zones, setZones] = useState([]);
+    const [plages, setPlages] = useState([]);
+    const [jours, setJours] = useState([]);
+    const joursDeLaSemaine = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+    const ordrecreneaux = ['9h-11h', '11h-14h', '14h-17h', '17h-20h', '20h-22h'];
+
+    useEffect(() => {
+        const fetchZonesData = async () => {
+            // get zones
+          if (localStorage.getItem('zones') == null || typeof(localStorage.getItem('zones')) == 'undefined') {
+            try {
+                const querySnapshot = await getDocs(collection(db, "zone_benevole"));
+                var listZones = [];
+                querySnapshot.forEach((doc) => {
+                    listZones.push({id: doc.id, data: doc.data()})
+                });
+                setZones(listZones)
+                localStorage.setItem('zones', JSON.stringify(listZones));
+            } catch (error) {
+                console.error('Error fetching zones data:', error);
+            }
+        }
+        else {
+            setZones(JSON.parse(localStorage.getItem('zones')));
+        }
+        }
+        fetchZonesData();
+    }, [val]);
+
+    useEffect(() => {
+        const fetchAffectZonesData = async () => {
+          // get affectations zones
+        if (localStorage.getItem('affectation_z') == null || typeof(localStorage.getItem('affectation_z')) == 'undefined') {
+            try {
+                const querySnapshot = await getDocs(collection(db, "affecter_zone"));
+                var listaffectZones = [];
+                querySnapshot.forEach((doc) => {
+                    listaffectZones.push({id: doc.id, data: doc.data()})
+                });
+                setAffectations_z(listaffectZones)
+            } catch (error) {
+                console.error('Error fetching zones data:', error);
+            }
+        }
+            else {
+                setPostes(JSON.parse(localStorage.getItem('affectation_z')));
+            }
+        }
+        fetchAffectZonesData();
+    }, [val]);
+
+    useEffect(() => {
+        const fetchPlagesData = async () => {
+        // get plages
+        try {
+            const querySnapshot = await getDocs(collection(db, "plage_horaire"));
+            var listPlage = []
+            var listPlage2 = []
+            var listJours = new Set()
+            querySnapshot.forEach((doc) => {
+                listPlage.push({id: doc.id, data: doc.data()})
+                listJours.add(doc.data().jour)
+            });
+
+          // classe les jours dans l'ordre
+            const joursNonOrdonnesArray = Array.from(listJours);
+            const joursOrdonnesResultat = joursNonOrdonnesArray.sort((a, b) => {
+              return joursDeLaSemaine.indexOf(a) - joursDeLaSemaine.indexOf(b);
+            });
+            
+            listJours = []
+            joursOrdonnesResultat.forEach((unjour) => {
+              listJours.push({"jour": unjour})
+            });
+            setJours(listJours);
+
+            // classe les plages horaires dans l'ordre
+            // parcourt les créneaux et les jours dans l'ordre puis les ajoute dans une 2e listePlage
+            jours.map((unjour)=>{
+              ordrecreneaux.forEach((creneau)=>{
+                  listPlage.map((uneplage)=>{
+                      if (uneplage.data.jour === unjour.jour && uneplage.data.horaire===creneau){ listPlage2.push(uneplage)}
+                  })
+              })
+            })
+            setPlages(listPlage2)
+        } catch (error) {
+            console.error('Error fetching postes data:', error);
+        }
+      }
+      fetchPlagesData();
+    }, [val]);
+
+    useEffect(() => {
+        const fetchPostesData = async () => {
+          // get postes
+        if (localStorage.getItem('postes') == null || typeof(localStorage.getItem('postes')) == 'undefined') {
+            try {
+                const querySnapshot = await getDocs(collection(db, "postes"));
+                var listPostes = []
+                querySnapshot.forEach((doc) => {
+                    listPostes.push({id: doc.id, data: doc.data()})
+                });
+                setPostes(listPostes)
+                localStorage.setItem('postes', JSON.stringify(listPostes));
+            } catch (error) {
+                console.error('Error fetching postes data:', error);
+            }
+          }
+          else {
+              setPostes(JSON.parse(localStorage.getItem('postes')));
+          }
+        }
+        fetchPostesData();
+    }, [val]);
 
     useEffect(() => {
         const fetchGamesData = async () => {
@@ -74,6 +191,8 @@ export default function HomeView(props){
         return () => getUserData(); 
     }, []);
 
+    console.log(affectations_z)
+
     const renderView = () => {
         switch (val) {
             case 0:
@@ -81,7 +200,7 @@ export default function HomeView(props){
             case 1:
                 return <PlanningView />;
             case 2:
-                return <RegisterPlanningView />;
+                return <RegisterPlanningView postes={postes} affectations_z={affectations_z} jours={jours} plages={plages} zones={zones}/>;
             case 3:
                 return <ChatView />;
             case 4:
--- front-app-volunteer/src/views/registerPlanningView/registerPlanningView.js
+++ front-app-volunteer/src/views/registerPlanningView/registerPlanningView.js
@@ -6,126 +6,27 @@ import './registerPlanningView.css';
 
 export default function RegisterPlanningView(props){
     useEffect(() => {
-        getData();
-        if (plages.length===0){getData()}
       },[]);
   
-        const [postes, setPostes] = useState([]);
-        const [affectations_z, setAffectations_z]=useState([]) // affectations zones
-        const [zones, setZones] = useState([]);
-        const [plages, setPlages] = useState([]);
-        const [jours, setJours] = useState([]);
-        const joursDeLaSemaine = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
-        const ordrecreneaux = ['9h-11h', '11h-14h', '14h-17h', '17h-20h', '20h-22h'];
-  
-  
-        const getData = async () => {
-          // get zones
-          if (localStorage.getItem('zones') == null || typeof(localStorage.getItem('zones')) == 'undefined') {
-            try {
-                const querySnapshot = await getDocs(collection(db, "zone_benevole"));
-                var listZones = [];
-                querySnapshot.forEach((doc) => {
-                    listZones.push({id: doc.id, data: doc.data()})
-                });
-                setZones(listZones)
-                localStorage.setItem('zones', JSON.stringify(listZones));
-            } catch (error) {
-                console.error('Error fetching zones data:', error);
-            }
-        }
-        else {
-            setZones(JSON.parse(localStorage.getItem('zones')));
-        }
+      const jours = props.jours;
+      const zones = props.zones;
+      const plages = props.plages;
+      const postes = props.postes;
+      const affectations_z = props.affectations_z
 
-        // get affectations zones
-        if (localStorage.getItem('affectation_z') == null || typeof(localStorage.getItem('affectation_z')) == 'undefined') {
-        try {
-            const querySnapshot = await getDocs(collection(db, "affecter_zone"));
-            var listaffectZones = [];
-            querySnapshot.forEach((doc) => {
-                listaffectZones.push({id: doc.id, data: doc.data()})
-            });
-            setAffectations_z(listaffectZones)
-        } catch (error) {
-            console.error('Error fetching zones data:', error);
-        }
-    }
-        else {
-            setPostes(JSON.parse(localStorage.getItem('postes')));
-        }
-  
-        // get postes
-        if (localStorage.getItem('postes') == null || typeof(localStorage.getItem('postes')) == 'undefined') {
-          try {
-              const querySnapshot = await getDocs(collection(db, "postes"));
-              var listPostes = []
-              querySnapshot.forEach((doc) => {
-                  listPostes.push({id: doc.id, data: doc.data()})
-              });
-              setPostes(listPostes)
-              localStorage.setItem('postes', JSON.stringify(listPostes));
-          } catch (error) {
-              console.error('Error fetching postes data:', error);
-          }
-        }
-        else {
-            setPostes(JSON.parse(localStorage.getItem('postes')));
-        }
-  
-        // get plages
-          try {
-              const querySnapshot = await getDocs(collection(db, "plage_horaire"));
-              var listPlage = []
-              var listPlage2 = []
-              var listJours = new Set()
-              querySnapshot.forEach((doc) => {
-                  listPlage.push({id: doc.id, data: doc.data()})
-                  listJours.add(doc.data().jour)
-              });
-  
-            // classe les jours dans l'ordre
-              const joursNonOrdonnesArray = Array.from(listJours);
-              const joursOrdonnesResultat = joursNonOrdonnesArray.sort((a, b) => {
-                return joursDeLaSemaine.indexOf(a) - joursDeLaSemaine.indexOf(b);
-              });
-              
-              listJours = []
-              joursOrdonnesResultat.forEach((unjour) => {
-                listJours.push({"jour": unjour})
-              });
-              setJours(listJours);
-
-              // classe les plages horaires dans l'ordre
-              // parcourt les créneaux et les jours dans l'ordre puis les ajoute dans une 2e listePlage
-              jours.map((unjour)=>{
-                ordrecreneaux.forEach((creneau)=>{
-                    listPlage.map((uneplage)=>{
-                        if (uneplage.data.jour === unjour.jour && uneplage.data.horaire===creneau){ listPlage2.push(uneplage)}
-                    })
-                })
-              })
-              setPlages(listPlage2)
-          } catch (error) {
-              console.error('Error fetching postes data:', error);
-          }
-        }
 
 
         // retourne le nombre d'inscrit a tel poste pour tel créneau
         const nbinscrits_zone = (id_creneau, zone_benevole) => { 
             var nb=0;
             affectations_z.map((affectation)=>{
-                console.log(affectation.data.id_plage, id_creneau, affectation.data.id_plage==id_creneau)
                 if (affectation.data.id_plage===id_creneau && affectation.data.zone===zone_benevole){
                     nb++
-                    console.log(nb);
                 }
             })
             return nb;
         }
 
-        console.log(jours)
       
         
       return(
@@ -215,7 +116,7 @@ export default function RegisterPlanningView(props){
                             <td>
                             <>{
                             }</>
-                               <button className='inscription'>{nbinscrits_zone(plage.id, unezone.intitule)}</button>
+                               <button className='inscription'>{nbinscrits_zone(plage.id, unezone.intitule)} / 2</button>
                             </td>
                             ))}
                       </tr>
