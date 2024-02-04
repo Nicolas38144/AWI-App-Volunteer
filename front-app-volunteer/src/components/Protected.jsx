f3bc9e3125df21e96b0ae700a7ded4602a60af63
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2024-02-05 00:51:11 +0100

    cookies

--- front-app-volunteer/src/components/Protected.jsx
+++ front-app-volunteer/src/components/Protected.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const Protected = () => {
-    const token = localStorage.getItem('token');
+    const token = getCookie('token')
+    
+    function getCookie(name) {
+        const cookies = document.cookie.split('; ');
+        for (const cookie of cookies) {
+            const [cookieName, cookieValue] = cookie.split('=');
+            if (cookieName === name) {
+                return cookieValue;
+            }
+        }
+        return null;
+    }
+
+    console.log(token)
 
     return (
         token ? <Outlet /> : <Navigate to="/login" />
